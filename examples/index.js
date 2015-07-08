2c2d071bb03385bb07e0c63cbb8c13250a267261
kgryte <kgryte@gmail.com>
2015-07-08 09:52:32 +0200

    [EXAMPLES] stdout.

--- examples/index.js
+++ examples/index.js
@@ -9,13 +9,17 @@ var data,
 	tmp,
 	i;
 
+// ----
 // Plain arrays...
 data = new Array( 10 );
 for ( i = 0; i < data.length; i++ ) {
 	data[ i ] = i * 0.5;
 }
 out = pdf( data );
+console.log( 'Arrays: %s\n', out );
 
+
+// ----
 // Object arrays (accessors)...
 function getValue( d ) {
 	return d.x;
@@ -28,7 +32,10 @@ for ( i = 0; i < data.length; i++ ) {
 out = pdf( data, {
 	'accessor': getValue
 });
+console.log( 'Accessors: %s\n', out );
 
+
+// ----
 // Deep set arrays...
 for ( i = 0; i < data.length; i++ ) {
 	data[ i ] = {
@@ -39,19 +46,38 @@ out = pdf( data, {
 	'path': 'x/1',
 	'sep': '/'
 });
+console.log( 'Deepset:');
+console.dir( out );
+console.log( '\n' );
 
+
+// ----
 // Typed arrays...
 data = new Int32Array( 10 );
 for ( i = 0; i < data.length; i++ ) {
 	data[ i ] = i;
 }
-out = pdf( data );
+tmp = pdf( data );
+out = '';
+for ( i = 0; i < data.length; i++ ) {
+	out += tmp[ i ];
+	if ( i < data.length-1 ) {
+		out += ',';
+	}
+}
+console.log( 'Typed arrays: %s\n', out );
 
+
+// ----
 // Matrices...
 mat = matrix( data, [5,2], 'int32' );
 out = pdf( mat );
+console.log( 'Matrix: %s\n', out.toString() );
 
+
+// ----
 // Matrices (custom output data type)...
 out = pdf( mat, {
 	'dtype': 'uint8'
 });
+console.log( 'Matrix (%s): %s\n', out.dtype, out.toString() );
